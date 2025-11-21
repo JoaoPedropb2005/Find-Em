0210bd4931f0e93aa4ec818b184a6b9ab442fade
a <a@gmail.com>
2025-11-21 10:50:01 -0300

    Pagina inicial de Welcome

--- app/src/main/java/com/example/findem/Explication1.2.kt
+++ app/src/main/java/com/example/findem/Explication1.2.kt
@@ -1,16 +1,10 @@
 package com.example.findem
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,7 +14,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.findem.ui.theme.FindEmTheme
 
 @Composable
 fun Explication1Page(
